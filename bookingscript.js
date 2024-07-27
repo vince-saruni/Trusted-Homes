d6af74bbfa1247d3ab7e4361bdcff9c7edb0bf20
Vince <69975841+vince-saruni@users.noreply.github.com>
2024-07-27 10:15:34 +0100

    Add files via upload

---
+++ bookingscript.js
@@ -0,0 +1,9 @@
+// Function to open the booking form
+function openBookingForm() {
+  document.getElementById("bookingForm").style.display = "block";
+}
+
+// Function to close the booking form
+function closeBookingForm() {
+  document.getElementById("bookingForm").style.display = "none";
+}
